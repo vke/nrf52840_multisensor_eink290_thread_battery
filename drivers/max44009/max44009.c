0bd4f586d8958bc2895c242e59b387129bee551b
Maxim <vke@users.noreply.github.com>
2022-01-29 03:15:15 +0200

    Small code refactor for max44009 driver

--- drivers/max44009/max44009.c
+++ drivers/max44009/max44009.c
@@ -21,13 +21,13 @@ static bool max44009_sensor_initialized = false;
 #define MAX44009_EXPONENT_OVERFLOW        15
 
 
-static int8_t user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+static ret_code_t user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, uint8_t address)
 {
-	ret_code_t err_code = nrf_drv_twi_tx(p_sensor_twi_instance, *(uint8_t *)intf_ptr, &reg_addr, 1, false);
+	ret_code_t err_code = nrf_drv_twi_tx(p_sensor_twi_instance, address, &reg_addr, 1, false);
 
 	APP_ERROR_CHECK(err_code);
 
-	err_code = nrf_drv_twi_rx(p_sensor_twi_instance, *(uint8_t *)intf_ptr, reg_data, len);
+	err_code = nrf_drv_twi_rx(p_sensor_twi_instance, address, reg_data, len);
 
 	APP_ERROR_CHECK(err_code);
 
@@ -55,24 +55,29 @@ ret_code_t max44009_start_measurement()
 
 	uint8_t data[2];
 
-	// FIXME: should be called in single transation without STOP
-	uint8_t i2c_result1 = user_i2c_read(MAX44009_REG_ADDR_LUX_HIGH, data, 1, &max44009_sensor_addr);
-	if (i2c_result1) {
+	// FIXME: should be called in one transation without STOP
+	ret_code_t i2c_result = NRF_ERROR_INTERNAL;
+
+	i2c_result = user_i2c_read(MAX44009_REG_ADDR_LUX_HIGH, data, 1, max44009_sensor_addr);
+	if (i2c_result != NRF_SUCCESS) {
 		results_handler(NULL);
 		return NRF_ERROR_INTERNAL;
 	}
-	uint8_t i2c_result2 = user_i2c_read(MAX44009_REG_ADDR_LUX_LOW, data + 1, 1, &max44009_sensor_addr);
-	if (i2c_result2) {
+
+	i2c_result = user_i2c_read(MAX44009_REG_ADDR_LUX_LOW, data + 1, 1, max44009_sensor_addr);
+	if (i2c_result != NRF_SUCCESS) {
 		results_handler(NULL);
 		return NRF_ERROR_INTERNAL;
 	}
 
 	uint8_t exponent = (data[0] >> 4) & 0x0F;
 	uint32_t mantissa = ((data[0] & 0x0F) << 4) + (data[1] & 0x0F);
+
 	max44009_results_t results;
-	results.lux = MAX44009_MAX_SENSOR_VALUE; // max sensor value - 188K
+	results.lux = MAX44009_MAX_SENSOR_VALUE;
 	if (exponent != MAX44009_EXPONENT_OVERFLOW)
 		results.lux = mantissa << exponent;
+
 	results_handler(&results);
 
 	return NRF_SUCCESS;
